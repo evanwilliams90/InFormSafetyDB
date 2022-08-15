28cf25bf1402d6dca6c184d4a3fbd449aca5b4a2
evanwilliams90 <erwilliams90@hotmail.co.nz>
2022-08-15 14:59:09 +1200

    Simplified databasehelper to insert & delete on one table at a time

--- app/src/main/java/com/example/informsafetydatabase/MainActivity.java
+++ app/src/main/java/com/example/informsafetydatabase/MainActivity.java
@@ -24,6 +24,9 @@ public class MainActivity extends AppCompatActivity {
     ListView lv_customerList;
     ArrayAdapter customerArrayAdapter;
     DatabaseHelper databaseHelper;
+    boolean success;
+    long teacherID;
+    long guardianID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,23 +36,31 @@ public class MainActivity extends AppCompatActivity {
         databaseHelper = new DatabaseHelper(MainActivity.this);
 
         // test insert
-        UserModel userModel = new UserModel(-1, 0, 0, "Teacher 1", "teacher1@gmail.com", "0210727600", "password01", "password01", true);
-        boolean success = databaseHelper.insertUser(userModel);
-
-        userModel = new UserModel(-1, 0, 0, "Teacher 2", "teacher2@gmail.com", "0210727598", "password02", "password01", true);
+        UserModel userModel = new UserModel(-1, -1, -1, "Teacher 1", "teacher1@gmail.com", "0210727600", "password01", "password01", true);
+        teacherID = databaseHelper.insertTeacher(userModel);
+        userModel.setTeacherID((int) teacherID);
         success = databaseHelper.insertUser(userModel);
 
-        userModel = new UserModel(-1, 0, 0, "Parent 1", "imaparent@gmail.com", "0270727676", "password03", "password01", false);
+        userModel = new UserModel(-1, -1, -1, "Teacher 2", "teacher2@gmail.com", "0210727598", "password02", "password01", true);
+        teacherID = databaseHelper.insertTeacher(userModel);
+        userModel.setTeacherID((int) teacherID);
         success = databaseHelper.insertUser(userModel);
 
-        userModel = new UserModel(-1, 0, 0, "Parent 2", "imaparenttoo@gmail.com", "0220727622", "password04", "password01", false);
+        userModel = new UserModel(-1, -1, -1, "Parent 1", "imaparent@gmail.com", "0270727676", "password03", "password01", false);
+        guardianID = databaseHelper.insertGuardian(userModel);
+        userModel.setGuardianID((int) guardianID);
+        success = databaseHelper.insertUser(userModel);
+
+        userModel = new UserModel(-1, -1, -1, "Parent 2", "imaparenttoo@gmail.com", "0220727622", "password04", "password01", false);
+        guardianID = databaseHelper.insertGuardian(userModel);
+        userModel.setGuardianID((int) guardianID);
         success = databaseHelper.insertUser(userModel);
 
 
         // test delete
-        userModel = new UserModel(0, 0, 1, "", "", "", "", "", false);
-        success = databaseHelper.deleteUser(userModel);
-
+        userModel = new UserModel(3, -1, 1, "", "", "", "", "", false);
+        databaseHelper.deleteUser(userModel);
+        databaseHelper.deleteGuardian(userModel);
 
         //Toast.makeText(MainActivity.this, userModel.toString(), Toast.LENGTH_SHORT).show();
 
